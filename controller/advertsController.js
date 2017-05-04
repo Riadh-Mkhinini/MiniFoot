0bea40f7bfe3d418cdfee1b605454708eba3b23c
Anas Sallem <anas.sallem73@gmail.com>
2017-05-04 16:09:11 +0100

    advertsController

--- controller/advertsController.js
+++ controller/advertsController.js
@@ -13,3 +13,17 @@ exports.getAllAdverts = (req,res) => {
     }
   });
 };
+
+exports.geAdvertsOfTeam = (req,res) => {
+Advert.find({createdBy:req.params.idEquipe}).limit(5).skip(req.query.page * 5).sort({ createdAt: -1 }).populate
+({
+    path:'createdBy',
+    select: ['_id', 'name', 'logo']
+}).exec(function(err,data) {
+  if (err) {
+    res.json({ success: false, message: 'Internal Server Error.' });
+  }else {
+    res.json(data);
+  }
+});
+};
--- routes/AdvertRouter.js
+++ routes/AdvertRouter.js
@@ -6,7 +6,8 @@ var routes = () => {
   var advertRouter = express.Router();
   // get all advert by page
   advertRouter.get('/', advertsController.getAllAdverts);
-
+  // get adverts of team
+  advertRouter.get('/:idEquipe', advertsController.geAdvertsOfTeam);
   return advertRouter;
 };
 
